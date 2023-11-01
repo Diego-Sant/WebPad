a2a2455b416599bf2e3bf05c67d65430d1a2dc52
Diego de Souza Santana <diegossantana068@gmail.com>
2023-11-01 16:55:58 -0300

    toolbar + icon functionality

--- app/(main)/(routes)/inicio/[documentId]/page.tsx
+++ app/(main)/(routes)/inicio/[documentId]/page.tsx
@@ -1,7 +1,35 @@
-const DocumentIdPage = () => {
+"use client";
+
+import { Toolbar } from "@/components/MainComponents/toolbar";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">,
+  }
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  });
+
+  if (document === undefined) {
+    return <p>Loading...</p>
+  }
+
+  if (document === null) {
+    return <div>Bloco de notas não encontrado.</div>
+  }
+
   return (
-    <div>
-      DocumentId
+    <div className="pt-40">
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
     </div>
   )
 }
